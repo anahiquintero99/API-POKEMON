01359d19e036ec5db576ee8926cbf73e78ee8640
Anahi Quintero <anitaquintero99@gmail.com>
2022-05-06 03:40:59 -0500

    create html, css and js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//URL API
+const API_URL = new URL('https://pokeapi.co/api/v2/pokemon/1');
+
+//global variables
+const titleList = document.querySelector('#title-list');
+const imgPokemon = document.querySelector('#img-pokemon');
+const namePokemon = document.querySelector('#name-pokemon');
+
+//Async Function
+async function reload() {
+  try {
+    //connecting api
+    const idPokemon = document.getElementById('id-pokemon').value;
+    const response = await fetch(API_URL + idPokemon);
+    const json = await response.json();
+
+    //found img
+    imgPokemon.src = json.sprites.other.dream_world.front_default;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+reload();
